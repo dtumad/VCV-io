790c155c700b4207ccd5d36de8d56a9d585518a3
Devon Tuma <dtumad@gmail.com>
2024-03-17 15:29:49 -0500

    build errors

--- VCV-io/OracleComp/DistSemantics/EvalDist.lean
+++ VCV-io/OracleComp/DistSemantics/EvalDist.lean
@@ -6,16 +6,20 @@ Authors: Devon Tuma
 import «VCV-io».OracleComp.DistSemantics.Support
 import Mathlib.Probability.ProbabilityMassFunction.Constructions
 
-namespace OracleComp
+open ENNReal BigOperators
 
-open ENNReal
+lemma Fintype.sum_inv_card (α : Type) [Fintype α] :
+  ∑ x : α, (fintype.card α)⁻¹ = 1 := by sorry
+
+namespace OracleComp
 
 variable {spec : OracleSpec} {α β : Type}
 
 noncomputable def evalDist : {α : Type} → OracleComp spec α → PMF α
 | _, pure' α a => PMF.pure a
 | _, query_bind' i t α oa =>
-    let unifQueryPMF := (PMF.ofFintype (λ _ ↦ 1 / Fintype.card (spec.range i)) (by
+    let unifQueryPMF := (PMF.ofFintype (λ _ ↦ (Fintype.card (spec.range i))⁻¹) (by
+
       simp only [Finset.card_univ, one_div, Finset.sum_const, nsmul_eq_mul]
       exact ENNReal.mul_inv_cancel (by simp) (nat_ne_top _)))
     PMF.bind unifQueryPMF (λ a ↦ evalDist $ oa a)
@@ -38,16 +42,18 @@ noncomputable example : ℝ≥0∞ := [(. + 1 = 5) | do let x ←$[0..4]; return
 
 section pure
 
-@[simp] lemma evalDist_pure (x : α) : evalDist (return x : OracleComp spec α) = pure x := rfl
+variable (x : α)
 
-@[simp] lemma probOutput_pure [DecidableEq α] (x y : α) :
+@[simp] lemma evalDist_pure : evalDist (return x : OracleComp spec α) = PMF.pure x := rfl
+
+@[simp] lemma probOutput_pure [DecidableEq α] (y : α) :
   [= y | (return x : OracleComp spec α)] = if y = x then 1 else 0 := by
   split_ifs with h
   · induction h
     exact PMF.pure_apply_self y
   · exact PMF.pure_apply_of_ne _ _ h
 
-@[simp] lemma probEvent_pure (x : α) (p : α → Prop) [DecidablePred p] :
+@[simp] lemma probEvent_pure (p : α → Prop) [DecidablePred p] :
   [p | (return x : OracleComp spec α)] = if p x then 1 else 0 :=
 (PMF.toOuterMeasure_pure_apply x p).trans (by congr)
 
@@ -55,7 +61,13 @@ end pure
 
 section bind
 
---
+variable (oa : OracleComp spec α) (ob : α → OracleComp spec β)
+
+@[simp] lemma evalDist_bind : evalDist (oa >>= ob) = (evalDist oa).bind (evalDist ∘ ob) := by
+  induction' oa using OracleComp.induction_on with α x i t α oa hoa
+  · simp []
+  · rw [bind_assoc, ← query_bind'_eq_query_bind, evalDist]
+
 end bind
 
 section query
