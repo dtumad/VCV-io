7a3612c97548e747fcae6fbf44ba23dde70e3647
Devon Tuma <dtumad@gmail.com>
2024-03-22 18:20:47 -0500

    fix import issue build error

--- VCVio.lean
+++ VCVio.lean
@@ -1,4 +1,5 @@
 import VCVio.OracleComp.DistSemantics.EvalDist
+import VCVio.OracleComp.DistSemantics.Seq
 import VCVio.OracleComp.DistSemantics.Support
 import VCVio.OracleComp.OracleComp
 import VCVio.OracleComp.OracleSpec
