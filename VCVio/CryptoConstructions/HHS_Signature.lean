f414c54df6998e61f7930fca2edab8de46674b21
Devon Tuma <dtumad@gmail.com>
2024-05-15 22:38:14 -0500

    build errors

--- VCVio/CryptoConstructions/HHS_Signature.lean
+++ VCVio/CryptoConstructions/HHS_Signature.lean
@@ -10,7 +10,7 @@ import Mathlib.Data.Vector.Zip
 import VCVio.CryptoConstructions.Fork
 
 
-import VCVio.OracleComp.Coercions.HasUnifSelect
+-- import VCVio.OracleComp.Coercions.HasUnifSelect
 import VCVio.OracleComp.Coercions.Append
 import VCVio.OracleComp.Constructions.Replicate
 
