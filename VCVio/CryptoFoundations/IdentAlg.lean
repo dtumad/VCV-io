153b957dd8ee882f1e8a1e4cde18cdda04edb6ca
Devon Tuma <dtumad@gmail.com>
2024-06-05 18:14:18 -0500

    rain

--- VCVio/CryptoFoundations/IdentAlg.lean
+++ VCVio/CryptoFoundations/IdentAlg.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Devon Tuma. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Devon Tuma
 -/
-import VCVio.CryptoFoundations.SecExp
+-- import VCVio.CryptoFoundations.SecExp
 
 
 /-!
@@ -13,6 +13,6 @@ This file defines a structure type for identification protocols.
 
 -/
 
-structure IdentAlg {ι : Type} (spec : OracleSpec ι)
-    extends OracleAlg spec :=
-  (test : OracleComp spec Unit)
+-- structure IdentAlg {ι : Type} (spec : OracleSpec ι)
+--     extends OracleAlg spec :=
+--   (test : OracleComp spec Unit)
