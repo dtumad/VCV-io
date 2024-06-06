29dad7923432e8d84eeea4e82bf945320bbb4e54
Devon Tuma <dtumad@gmail.com>
2024-06-06 15:43:02 -0500

    adjust sigma alg definition

--- VCVio/CryptoFoundations/SigmaAlg.lean
+++ VCVio/CryptoFoundations/SigmaAlg.lean
@@ -12,3 +12,16 @@ This file defines a structure type for identification protocols.
 -/
 
 open OracleSpec OracleComp ENNReal
+
+/-- A sigma protocol for statements in `X`, witnesses in `W`,
+prover messages in `M`, verifier challenges in `C`,
+and proofs in `P`.
+`p` is the relation on statements and witnesses to be proven.
+
+We leave properties like special soundness as seperate definitions for better modularity.-/
+structure SigmaAlg {ι : Type} (spec : ℕ → OracleSpec ι)
+    (X W M C P : ℕ → Type) (p : (sp : ℕ) → X sp → W sp)
+    extends OracleAlg spec where
+  gen (sp : ℕ) : X sp → W sp → OracleComp (spec sp) (M sp)
+  prove (sp : ℕ) : X sp → W sp → M sp → C sp → OracleComp (spec sp) (P sp)
+  verify (sp : ℕ) : X sp → M sp → C sp → P sp → OracleComp (spec sp) Bool
