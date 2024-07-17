ca268fcbc0aa4cd8043334a52eed98580fd8bcd7
Devon Tuma <dtumad@gmail.com>
2024-07-17 01:32:32 -0500

    fix: tweaks to make the protocol framework work smoother (#13)
    tweaks to make the protocol framework work smoother

--- Examples/OneTimePad.lean
+++ Examples/OneTimePad.lean
@@ -12,20 +12,20 @@ import Mathlib.Data.Vector.Zip
 This file defines and proves the perfect secrecy of the one-time pad encryption algorithm.
 -/
 
-open OracleSpec OracleComp OracleAlg ENNReal BigOperators
+open Mathlib OracleSpec OracleComp OracleAlg ENNReal BigOperators
 
 def oneTimePad : SymmEncAlg (λ _ ↦ unifSpec)
     (Vector Bool) (Vector Bool) (Vector Bool) where
   keygen := λ sp ↦ $ᵗ Vector Bool sp -- random bitvec
   encrypt := λ _ m k ↦ return m.zipWith xor k
   decrypt := λ _ σ k ↦ return σ.zipWith xor k
-  __ := baseOracleAlg -- leave oracles as is
+  __ := baseOracleAlg -- Oracles already reduced
 
 namespace oneTimePad
 
--- theorem isSound (n : ℕ) : (oneTimePad).isSound := by
---   have : ∀ ys xs : Vector Bool n, (ys.zipWith xor xs).zipWith xor xs = ys :=
---     λ ys xs ↦ Vector.ext (λ i ↦ by simp)
-  -- simpa [oneTimePad, SymmEncAlg.soundnessExp] using this
+theorem isSound : (oneTimePad).isSound := by
+  suffices ∀ n (ys xs : Vector Bool n), (ys.zipWith xor xs).zipWith xor xs = ys
+  by simpa [oneTimePad, SymmEncAlg.soundnessExp] using this
+  exact λ n ys xs ↦ Vector.ext (λ i ↦ by simp)
 
 end oneTimePad
