72a40cd9e0deb2d0e97ac8a9f92df723d8b32aec
Devon Tuma <dtumad@gmail.com>
2024-08-13 21:32:52 -0500

    literal counting

--- VCVio/OracleComp/SimSemantics/QueryTracking/CountingOracle.lean
+++ VCVio/OracleComp/SimSemantics/QueryTracking/CountingOracle.lean
@@ -55,7 +55,8 @@ lemma support_simulate (oa : OracleComp spec α) (qc : ι → ℕ) :
 
 /-- Reduce membership in the support of simulation with counting to membership in simulation
 starting with the count at `0`.
-TODO: lemmas like this suggest maybe support shouldn't auto reduce on the computation type? -/
+TODO: lemmas like this suggest maybe support shouldn't auto reduce on the computation type?
+TODO: implicit parameters, and add extra helper lemmas -/
 lemma mem_support_simulate_iff (oa : OracleComp spec α) (qc : ι → ℕ) (z : α × (ι → ℕ)) :
     z ∈ (simulate countingOracle qc oa).support ↔
       ∃ qc', (z.1, qc') ∈ (simulate countingOracle 0 oa).support ∧ qc + qc' = z.2 := by
@@ -97,118 +98,112 @@ lemma mem_support_snd_map_simulate_iff_of_le (oa : OracleComp spec α) {qc qc' :
       qc' - qc ∈ (Prod.snd <$> simulate countingOracle 0 oa).support := by
   simp only [mem_support_snd_map_simulate_iff, zero_add]
   refine exists_congr (λ qc'' ↦ exists_congr (λ x ↦ ?_))
-  refine and_congr_right' ?_
-  refine ⟨λ h ↦ funext (λ x ↦ ?_), λ h ↦ funext (λ x ↦ ?_)⟩
-  · rw [← h]
-    refine symm ?_
-    refine Nat.add_sub_cancel_left _ _
-  · rw [h]
-    refine Nat.add_sub_cancel' (hqc x)
+  refine and_congr_right' ⟨λ h ↦ funext (λ x ↦ ?_), λ h ↦ funext (λ x ↦ ?_)⟩
+  · simp only [← h, Pi.sub_apply, Pi.add_apply, add_tsub_cancel_left]
+  · simp [h, Nat.add_sub_cancel' (hqc x)]
 
-lemma le_of_mem_support_simulate {oa : OracleComp spec α} {qc qc' : ι → ℕ} {x : α}
-    (h : (x, qc') ∈ (simulate countingOracle qc oa).support) : qc ≤ qc' := by
+lemma le_of_mem_support_simulate {oa : OracleComp spec α} {qc : ι → ℕ} {z : α × (ι → ℕ)}
+    (h : z ∈ (simulate countingOracle qc oa).support) : qc ≤ z.2 := by
   rw [mem_support_simulate_iff] at h
   obtain ⟨qc'', _, h⟩ := h
   exact le_of_le_of_eq le_self_add h
+
+lemma add_mem_support_simulate {oa : OracleComp spec α} {qc : ι → ℕ} {z : α × (ι → ℕ)}
+    (hz : z ∈ (simulate countingOracle qc oa).support) (qc' : ι → ℕ) :
+    (z.1, z.2 + qc') ∈ (simulate countingOracle (qc + qc') oa).support := by
+  obtain ⟨qc1, hqc', h⟩ := (mem_support_simulate_iff _ _ _).1 hz
+  exact (mem_support_simulate_iff _ _ _).2 ⟨qc1, hqc', h ▸ by ring⟩
 
 lemma mem_support_simulate_pure_iff (x : α) (qc : ι → ℕ) (z : α × (ι → ℕ)) :
     z ∈ (simulate countingOracle qc (pure x : OracleComp spec α)).support ↔ z = (x, qc) := by
   simp only [simulate_pure, support_pure, Set.mem_singleton_iff]
 
+lemma apply_ne_zero_of_mem_support_simulate_queryBind {i : ι} {t : spec.domain i}
+    {oa : spec.range i → OracleComp spec α} {qc : ι → ℕ} {z : α × (ι → ℕ)}
+    (hz : z ∈ (simulate countingOracle qc (query i t >>= oa)).support) : z.2 i ≠ 0 := by
+  rw [mem_support_simulate_iff, simulate_query_bind] at hz
+  rw [support_bind] at hz
+  simp at hz
+  obtain ⟨qc', ⟨⟨u, hu⟩, hqc⟩⟩ := hz
+  have := le_of_mem_support_simulate hu i
+  simp at this
+  refine Nat.pos_iff_ne_zero.1 ?_
+  rw [← hqc, Pi.add_apply]
+  refine Nat.add_pos_right ?_ ?_
+  refine Nat.lt_of_succ_le this
+
+lemma exists_mem_support_of_mem_support_simulate_queryBind {i : ι} {t : spec.domain i}
+    {oa : spec.range i → OracleComp spec α} {qc : ι → ℕ} {z : α × (ι → ℕ)}
+    (hz : z ∈ (simulate countingOracle qc (query i t >>= oa)).support) :
+    ∃ u, (z.1, Function.update z.2 i (z.2 i - 1)) ∈
+      (simulate countingOracle qc (oa u)).support := by
+  rw [mem_support_simulate_iff, simulate_query_bind, support_bind] at hz
+  simp at hz
+  obtain ⟨qc', ⟨⟨u, hu⟩, hqc⟩⟩ := hz
+  refine ⟨u, ?_⟩
+  simp [← hqc]
+  have hqc' : qc' i ≠ 0 := by {
+    have := le_of_mem_support_simulate hu i
+    refine Nat.pos_iff_ne_zero.1 ?_
+    refine lt_of_lt_of_le ?_ this
+    simp only [update_same, zero_lt_one]
+  }
+  rw [mem_support_simulate_iff_of_le]
+  · simp
+    rw [mem_support_simulate_iff_of_le] at hu
+    · simp at hu
+      convert hu using 2
+      refine funext (λ j ↦ ?_)
+      by_cases hj : j = i
+      · simp [hj]
+        refine Nat.sub_eq_of_eq_add ?_
+        rw [add_comm _ (qc i)]
+        rw [Nat.add_sub_assoc]
+        rw [Nat.one_le_iff_ne_zero]
+
+        exact hqc'
+      · simp [hj]
+    · intro j
+      by_cases hj : j = i
+      · induction hj
+        simp only [update_same]
+        have := le_of_mem_support_simulate hu j
+        refine le_trans ?_ this
+        simp
+      · simp [hj]
+  · intro j
+    by_cases hj : j = i
+    · induction hj
+      simp only [update_same]
+      rw [Nat.le_sub_one_iff_lt]
+      · refine Nat.lt_add_of_pos_right ?_
+        rw [pos_iff_ne_zero]
+        assumption
+      · refine Nat.add_pos_right _ ?_
+        rwa [pos_iff_ne_zero]
+    · simp [hj]
+
 lemma mem_support_simulate_queryBind_iff (i : ι) (t : spec.domain i)
     (oa : spec.range i → OracleComp spec α) (qc : ι → ℕ) (z : α × (ι → ℕ)) :
     z ∈ (simulate countingOracle qc (query i t >>= oa)).support ↔ z.2 i ≠ 0 ∧ ∃ u,
       (z.1, Function.update z.2 i (z.2 i - 1)) ∈ (simulate countingOracle qc (oa u)).support := by
-  rw [mem_support_simulate_iff, simulate_query_bind]
-  rw [support_bind]
-  simp only [apply_eq, Pi.zero_apply, zero_add, support_map, support_query, Set.image_univ,
-    Set.mem_range, Set.iUnion_exists, Set.iUnion_iUnion_eq', Set.mem_iUnion]
-  refine ⟨λ h ↦ ?_, λ h ↦ ?_⟩
-  · obtain ⟨qc', ⟨⟨u, hu⟩, hqc⟩⟩ := h
-    have hqc' : qc' i ≠ 0 := by {
-      have := le_of_mem_support_simulate hu i
-      refine Nat.pos_iff_ne_zero.1 ?_
-      refine lt_of_lt_of_le ?_ this
-      simp only [update_same, zero_lt_one]
-    }
-    refine ⟨?_, ?_⟩
-    · have := le_of_mem_support_simulate hu i
-      rw [← hqc, Pi.add_apply]
-      rw [← Nat.pos_iff_ne_zero]
-      refine Nat.add_pos_right _ ?_
-      refine lt_of_lt_of_le ?_ this
-      simp
-    · refine ⟨u, ?_⟩
-      simp [← hqc]
-      rw [mem_support_simulate_iff_of_le]
-      · simp
-
-        rw [mem_support_simulate_iff_of_le] at hu
-        · simp at hu
-          convert hu using 2
-
-          refine funext (λ j ↦ ?_)
-          by_cases hj : j = i
-          · simp [hj]
-            refine Nat.sub_eq_of_eq_add ?_
-
-            rw [add_comm _ (qc i)]
-            rw [Nat.add_sub_assoc]
-            rw [Nat.one_le_iff_ne_zero]
-            exact hqc'
-          · simp [hj]
-        · intro j
-
-          by_cases hj : j = i
-          · induction hj
-            simp only [update_same]
-            have := le_of_mem_support_simulate hu j
-            refine le_trans ?_ this
-            simp
-          · simp [hj]
-      · intro j
-        by_cases hj : j = i
-        · induction hj
-          simp only [update_same]
-          rw [Nat.le_sub_one_iff_lt]
-          · refine Nat.lt_add_of_pos_right ?_
-            rw [pos_iff_ne_zero]
-            assumption
-          · refine Nat.add_pos_right _ ?_
-            rwa [pos_iff_ne_zero]
-        · simp [hj]
-  · obtain ⟨hz, ⟨u, hu⟩⟩ := h
-
-    refine ⟨?_, ⟨⟨u, ?_⟩, ?_⟩⟩
-    · refine z.2 - ?_
-      refine qc
-    · rw [mem_support_simulate_iff_of_le] at hu
-      · simp only at hu
-
-        rw [mem_support_simulate_iff_of_le]
-        · simp only
-          convert hu using 2
-          refine funext (λ j ↦ ?_)
-          by_cases hj : j = i
-          · simp [hj]
-            exact Nat.sub_right_comm (z.2 i) (qc i) 1
-          · simp [hj]
-        · intro j
-          rw [mem_support_simulate_iff_of_le] at hu
-          · by_cases hj : j = i
-            · induction hj
-              simp only [update_same, Pi.sub_apply]
-              rw [Nat.one_le_iff_ne_zero]
-              simp only [Nat.sub_zero] at hu
-              have := le_of_mem_support_simulate hu j
-              simp at this
-              rw [Nat.sub_ne_zero_iff_lt]
-              sorry
-            sorry
-          · sorry
-      · sorry
-    · sorry
-
+  refine ⟨λ h ↦ ⟨?_, ?_⟩, λ h ↦ ?_⟩
+  · refine apply_ne_zero_of_mem_support_simulate_queryBind h
+  · refine exists_mem_support_of_mem_support_simulate_queryBind h
+  · obtain ⟨hz0, ⟨u, hu⟩⟩ := h
+    simp
+    refine ⟨u, ?_⟩
+    have := add_mem_support_simulate hu (update 0 i 1)
+    convert this
+    · refine funext (λ j ↦ symm ?_)
+      by_cases hij : j = i
+      · simpa [hij] using Nat.succ_pred hz0
+      · simp [hij]
+    · refine funext (λ j ↦ ?_)
+      by_cases hij : j = i
+      · induction hij
+        simp
+      · simp [hij]
 
 end support
 
