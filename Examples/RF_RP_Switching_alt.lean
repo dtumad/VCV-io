9e99668c543b9753450700954e4aa83e6e9f0600
Devon Tuma <dtumad@gmail.com>
2024-09-11 17:38:11 -0500

    fixing append??

--- Examples/RF_RP_Switching_alt.lean
+++ Examples/RF_RP_Switching_alt.lean
@@ -9,8 +9,6 @@ import VCVio
 # Random-Function Random-Permutation Switching Lemma
 -/
 
-
-
 open OracleSpec OracleComp
 
 /-- Security adversary for RF-RP distinguisher experiments.
@@ -39,15 +37,47 @@ noncomputable def RP_Exp (adv : RF_RP_Adv α) :
     let (u, cache') ← cache.lookup_or_else () t ($ usedᶜ)
     return (u, cache', insert u used)
 
+instance  {ι ι' τ : Type} {spec : OracleSpec ι} {spec' : OracleSpec ι'}
+    {source_spec : OracleSpec τ} {σ : Type}
+    [∀ α, Coe (OracleComp spec α) (OracleComp spec' α)] :
+    Coe (source_spec →[σ]ₛₒ spec) (source_spec →[σ]ₛₒ spec') where
+  coe := λ so i t s ↦ ↑(so i t s)
+
+section parallelAppend
+
+def parallelAppend {ι₁ ι₂ ι₃ ι₄ : Type}
+    {spec₁ : OracleSpec ι₁} {spec₂ : OracleSpec ι₂}
+    {spec₃ : OracleSpec ι₃} {spec₄ : OracleSpec ι₄} {σ τ : Type}
+    (so : spec₁ →[σ]ₛₒ spec₂) (so' : spec₃ →[τ]ₛₒ spec₄) :
+    spec₁ ++ₒ spec₃ →[σ × τ]ₛₒ spec₂ ++ₒ spec₄ :=
+  ↑so ++ₛₒ ↑so'
+
+infixl : 65 " ||ₛₒ " => parallelAppend
+
+end parallelAppend
+
+
+-- TODO: The inference rules with parallel append and interactions needing this
+def simulate'' {ι ιₜ : Type} {spec : OracleSpec ι} {specₜ : OracleSpec ιₜ}
+    {α σ : Type} (oa : OracleComp spec α) (so : spec →[σ]ₛₒ specₜ) (s : σ) :
+  OracleComp specₜ α :=
+  Prod.fst <$> simulate so s oa
+
+example {α : Type} (oa : OracleComp unifSpec α) :
+    OracleComp (unifSpec ++ₒ emptySpec) α := ↑oa
+
+example (so : unifSpec →[Unit]ₛₒ unifSpec) :
+    unifSpec →[Unit]ₛₒ unifSpec ++ₒ []ₒ := ↑so
+
 -- Old version manually
 noncomputable def RF_RP_Exp (adv : RF_RP_Adv α) [∀ n, SelectableType (α n → α n)]
-    [∀ n, SelectableType {f : α n → α n // f.Bijective}] :  SecExp (λ _ ↦ []ₒ) where
+    [∀ n, SelectableType {f : α n → α n // f.Bijective}] : SecExp (λ _ ↦ []ₒ) where
   main n := do
     let f ←$ᵗ (α n → α n) -- random function
     let g : {f : α n → α n // f.Bijective} ←$ᵗ {f : α n → α n // f.Bijective} -- random perm
     let b : Bool ←$ᵗ Bool
     let h : α n → α n := if b then f else g.1
-    let so : (α n →ₒ α n) →[Unit]ₛₒ unifSpec ++ₒ []ₒ :=
+    let so : (α n →ₒ α n) →[Unit]ₛₒ []ₒ :=
       λ _ t () ↦ return (h t, ())
-    simulate' (idOracle ++ₛₒ so) ((), ()) (adv.run n ())
+    simulate' (idOracle ||ₛₒ so) ((), ()) (adv.run n ())
   __ := OracleAlg.baseOracleAlg
