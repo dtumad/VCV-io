7926dbe6ff31f522a32aa3c13504fba0f7bea2ec
Devon Tuma <dtumad@gmail.com>
2024-09-18 11:53:20 -0500

    temp

--- Examples/RF_RP_Switching_alt.lean
+++ Examples/RF_RP_Switching_alt.lean
@@ -22,6 +22,9 @@ def RF_RP_Adv (α : ℕ → Type) [∀ n, Fintype (α n)]
     [∀ n, Inhabited (α n)] [∀ n, DecidableEq (α n)] :=
   SecAdv (λ n ↦ (α n →ₒ α n)) (λ _ ↦ Unit) (λ _ ↦ Bool)
 
+def distinguisher {ι : Type} [DecidableEq ι] (spec : ℕ → OracleSpec ι) :=
+  SecAdv spec (λ _ ↦ Unit) (λ _ ↦ Bool)
+
 variable {α : ℕ → Type} [∀ n, Fintype (α n)]
     [∀ n, Inhabited (α n)] [∀ n, DecidableEq (α n)]
     [∀ n, SelectableType (α n)]
@@ -33,6 +36,19 @@ def RF_Exp (adv : RF_RP_Adv α) : SecExp (λ n ↦ (α n →ₒ α n)) where
   baseState n := QueryCache (α n →ₒ α n)
   init_state _ := ∅
   baseSimOracle _ := randOracle
+
+
+
+def distinguisher_exp {ι : Type} [DecidableEq ι] (spec : ℕ → OracleSpec ι)
+    {ι' : Type} (spec' : ℕ → OracleSpec ι')
+    (adv : distinguisher spec) {σ₁ σ₂ : Type}
+    (so : (n : ℕ) → spec n →[σ₁]ₛₒ unifSpec)
+    (so' : (n : ℕ) → spec n →[σ₂]ₛₒ unifSpec)
+    (s₁ : ℕ → σ₁) (s₂ : ℕ → σ₂) : SecExp spec where
+  baseState := _
+  init_state := s₁
+  baseSimOracle := so
+  main := sorry
 
 /-- Run a `RF_RP_Adv` using a random permutation to answer queries.
 The simulation uses an additional finset to avoid choosing duplicate query outputs.
@@ -53,8 +69,9 @@ lemma state_thing {α ι₁ ι₂: Type} {spec : OracleSpec ι₁} {spec' : Orac
     (oa : OracleComp spec Bool)
     {σ₁ σ₂ : Type} (so : spec →[σ₁]ₛₒ spec') (so' : spec →[σ₂]ₛₒ spec')
     (good_step₁ : σ₁ → Prop) (good_step₂ : σ₂ → Prop)
-    (s₁ : σ₁) (s₂ : σ₂) (ε₁ ε₂ : ℝ≥0∞)
-    : [= true | simulate' so s₁ oa] + [= false | simulate' so' s₂ oa] ≤ ε₁ + ε₂ := sorry
+    (s₁ : σ₁) (s₂ : σ₂) (ε₁ ε₂ : ℝ≥0∞) :
+    [= true | simulate' so s₁ oa] + [= false | simulate' so' s₂ oa] ≤ ε₁ + ε₂ :=
+  sorry
 
 lemma strong_interpolation_theorem {α ι₁ ι₂: Type} [DecidableEq ι₁] [DecidableEq ι₂]
     {spec : OracleSpec ι₁} {spec' : OracleSpec ι₂}
