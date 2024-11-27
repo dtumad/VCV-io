b538a9b3f1e6ab03f7a7471ab886af57c01466f4
Devon Tuma <dtumad@gmail.com>
2024-11-27 14:07:48 -0600

    better api for fork

--- VCVio/CryptoFoundations/Fork.lean
+++ VCVio/CryptoFoundations/Fork.lean
@@ -13,7 +13,7 @@ import VCVio.OracleComp.Coercions.Append
 
 -/
 
-open OracleSpec OracleComp Option ENNReal
+open OracleSpec OracleComp Option ENNReal Function
 
 -- structure ForkAdv {ι : Type} (spec : OracleSpec ι) (α β : Type) (i : ι)
 --     extends SecAdv spec α β where
@@ -50,42 +50,59 @@ variable [∀ i, SelectableType (spec.range i)]
 
 def fork [unifSpec ⊂ₒ spec] (oa : OracleComp spec α) (qb : ι → ℕ)
     (activeOracles : List ι) (i : ι)
-    (cf : α → Option (Fin (qb i + 1))) :
+    (cf : α → QueryLog spec → Option (Fin (qb i + 1))) :
     OracleComp spec (Option (α × α × Fin (qb i + 1))) := do
   let s : Fin (qb i + 1) ← $[0..qb i]
-  let sharedSeed : QuerySeed spec ← generateSeed spec (Function.update qb i s) activeOracles
+  let sharedSeed : QuerySeed spec ← generateSeed spec (update qb i s) activeOracles
   let seed₁ ← sharedSeed.addValue i <$> ($ᵗ spec.range i)
   let seed₂ ← sharedSeed.addValue i <$> ($ᵗ spec.range i)
-  let y₁ ← simulate' seededOracle seed₁ oa
-  let y₂ ← simulate' seededOracle seed₂ oa
-  return if cf y₁ = some s ∧ cf y₂ = some s ∧ (seed₁ i).getI s ≠ (seed₂ i).getI s
-    then some (y₁, y₂, s) else none
+  let (x₁, (log₁, _)) ← simulate (seededOracle ∘ₛₒ loggingOracle) (∅, seed₁) oa
+  let (x₂, (log₂, _)) ← simulate (seededOracle ∘ₛₒ loggingOracle) (∅, seed₂) oa
+  return if cf x₁ log₁ = some s ∧ cf x₂ log₂ = some s ∧
+    (seed₁ i).getI s ≠ (seed₂ i).getI s then some (x₁, x₂, s) else none
 
 variable [unifSpec ⊂ₒ spec] (oa : OracleComp spec α) (qb : ι → ℕ)
     (js : List ι) (i : ι)
-    (cf : α → Option (Fin (qb i + 1)))
+    (cf : α → QueryLog spec → Option (Fin (qb i + 1)))
 
 /-- Proof of non-negligible lower bound on the failure chance of forking a computation
 succeeding in producing a result. By the filtering in the final `ite` this bounds the
 chance of getting a result with the desired forking semantics. -/
 theorem le_probEvent_isSome_fork :
     let frk := [λ z ↦ z.isSome | fork oa qb js i cf]
-    let acc := [λ z ↦ z.isSome | cf <$> oa]
+    let acc := [λ (x, log) ↦ (cf x log).isSome | simulate loggingOracle ∅ oa]
     let q : ℝ≥0∞ := qb i
     let h : ℝ≥0∞ := Fintype.card (spec.range i)
     (acc / q) ^ 2 - acc / h ≤ frk := by
   sorry
 
-/-- Theorem expressing the most useful properties of successful forking.
+-- /-- Succesfull outputs of `fork oa qb js i cf` are outputs of running `oa` with a seeded oracle
+-- where the seeds agree up until query `s` to the oracle at index `i`, at which point
+-- they give distinct outputs to the query. Additionally the `cf` function chooses `s`
+-- as the index on both of the outputs. -/
+-- lemma some_mem_support_fork_iff (x₁ x₂ : α) (s : Fin (qb i + 1)) :
+--     some (x₁, x₂, s) ∈ (fork oa qb js i cf).support ↔
+--       cf x₁ = some s ∧ cf x₂ = some s ∧
+--       ∃ shared_seed ∈ (generateSeed spec (update qb i s) js).support,
+--         ∃ u₁ u₂ : spec.range i, u₁ ≠ u₂ ∧
+--           x₁ ∈ (simulate' seededOracle (shared_seed.addValue i u₁) oa).support ∧
+--           x₂ ∈ (simulate' seededOracle (shared_seed.addValue i u₂) oa).support :=
+--   sorry
+
+/-- Theorem expressing the most useful properties of successful forking,
+framed in terms of `loggingOracle` instead of `seededOracle`.
 Weaker than the full characterization of the `support` of fork,
 but often more useful in practice -/
 theorem exists_log_of_mem_support_fork (x₁ x₂ : α) (s : Fin (qb i + 1))
+    (hcf : ∀ x log, (cf x log).isSome → ↑s < (log i).length)
     (h : some (x₁, x₂, s) ∈ ((fork oa qb js i cf)).support) :
-    ∃ log₁ : QuerySeed spec, ∃ log₂ : QuerySeed spec,
-      (log₁ i).getI s ≠ (log₂ i).getI s ∧
-      cf x₁ = some s ∧ cf x₂ = some s ∧
-      x₁ ∈ (simulate' loggingOracle ∅ oa).support ∧
-      x₂ ∈ (simulate' loggingOracle ∅ oa).support :=
-  sorry
+    ∃ log₁ : QueryLog spec, ∃ log₂ : QueryLog spec,
+      ∃ h₁ : cf x₁ log₁ = some s, ∃ h₂ : cf x₂ log₂ = some s,
+      let hcf₁ : ↑s < (log₁ i).length := hcf x₁ log₁ (h₁ ▸ isSome_some)
+      let hcf₂ : ↑s < (log₂ i).length := hcf x₂ log₂ (h₂ ▸ isSome_some)
+      ((log₁ i)[s]'hcf₁).1 = ((log₂ i)[s]'hcf₂).1 ∧
+      ((log₁ i)[s]'hcf₁).2 ≠ ((log₂ i)[s]'hcf₂).2 ∧
+      (x₁, log₁) ∈ (simulate loggingOracle log₁ oa).support ∧
+      (x₂, log₂) ∈ (simulate loggingOracle log₂ oa).support := sorry
 
 end OracleComp
