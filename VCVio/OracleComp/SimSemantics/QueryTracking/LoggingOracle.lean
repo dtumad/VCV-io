949c96776e8416c83f8bf2396da193293fddf591
Quang Dao <quangdao@umich.edu>
2025-02-04 13:53:09 -0500

    `WriterT` instance for `QueryLog`

--- VCVio/OracleComp/SimSemantics/QueryTracking/LoggingOracle.lean
+++ VCVio/OracleComp/SimSemantics/QueryTracking/LoggingOracle.lean
@@ -26,8 +26,18 @@ def QueryLog (spec : OracleSpec ι) : Type :=
 def QueryLog' (spec : OracleSpec ι) : Type :=
   List (Σ i : ι, spec.domain i × spec.range i)
 
+namespace QueryLog
+
 instance : EmptyCollection (QueryLog spec) := ⟨λ _ ↦ []⟩
 instance : Append (QueryLog spec) := ⟨λ log log' i ↦ (log i) ++ (log' i)⟩
+instance : Monoid (QueryLog spec) where
+  mul := instAppend.append
+  one := instEmptyCollection.emptyCollection
+  mul_assoc := fun a b c => by sorry
+  one_mul := sorry
+  mul_one := sorry
+
+end QueryLog
 
 instance : EmptyCollection (QueryLog' spec) := ⟨[]⟩
 instance : Append (QueryLog' spec) := ⟨List.append⟩
@@ -44,8 +54,8 @@ def logQuery (log : QueryLog spec) {i : ι}
 
 end logQuery
 
-def singleton {i : ι} (t : spec.domain i) (u : spec.range i) : QueryLog' spec :=
-  [⟨i, (t, u)⟩]
+def singleton {i : ι} (t : spec.domain i) (u : spec.range i) : QueryLog spec :=
+  Function.update (λ _ ↦ []) i [(t, u)]
 
 /-- Check if an element was ever queried in a log of queries.
 Relies on decidable equality of the domain types of oracles. -/
@@ -56,20 +66,29 @@ def wasQueried [spec.DecidableEq] (log : QueryLog spec) (i : ι) (t : spec.domai
 
 end QueryLog
 
+def QueryLog'.singleton {i : ι} (t : spec.domain i) (u : spec.range i) : QueryLog' spec :=
+  [⟨i, (t, u)⟩]
+
 end OracleSpec
 
 open OracleComp OracleSpec
 
-def loggingOracle' : SimOracle' spec spec (WriterT (QueryLog' spec)) where
+def loggingOracleNew : SimOracle' spec spec (WriterT (QueryLog spec)) where
   impl | query i t => do
     let u ← query i t
     tell (QueryLog.singleton t u)
     return u
 
+def loggingOracle' : SimOracle' spec spec (WriterT (QueryLog' spec)) where
+  impl | query i t => do
+    let u ← query i t
+    tell (QueryLog'.singleton t u)
+    return u
+
 def logingOracle'' : QueryImpl spec (WriterT (QueryLog' spec) (OracleComp spec)) where
   impl | query i t => do
     let u ← query i t
-    tell (QueryLog.singleton t u)
+    tell (QueryLog'.singleton t u)
     return u
 
 -- `(22, [⟨20, ((), 8)⟩, ⟨20, ((), 14)⟩])`
