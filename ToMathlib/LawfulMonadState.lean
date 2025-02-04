58a1e91d505f2858f2e7fa9f55863f31abbfb2af
Quang Dao <quangdao@umich.edu>
2025-02-03 22:02:02 -0500

    more progress on `LawfulMonadState`

--- ToMathlib/LawfulMonadState.lean
+++ ToMathlib/LawfulMonadState.lean
@@ -5,16 +5,15 @@ Authors: Devon Tuma
 -/
 -- import VCVio
 import Mathlib.Control.Monad.Basic
+import Init.Control.State
 
 /-!
 # Lawful MonadState
 
 This file defines the `LawfulMonadState` class, which adds laws to the `MonadState` class.
+
+TODO: what about `LawfulMonad{Control/Reader/Writer/Option}`?
 -/
-
-#check MonadStateOf
-
-#check MonadState
 
 universe u v
 
@@ -24,14 +23,15 @@ class LawfulMonadStateOf (σ : outParam (Type u)) (m : Type u → Type v) extend
   modifyGet_eq : {α : Type u} → (f : σ → α × σ) →
     modifyGet f = do let (a, s) := f (← get); set s; pure a
 
-export LawfulMonadStateOf (modifyGet_eq)
-
 -- Should this be simp?
 attribute [simp] LawfulMonadStateOf.modifyGet_eq
 
-section LawfulMonadStateOf
+namespace LawfulMonadStateOf
 
 variable {σ : Type u} {m : Type u → Type v}
+
+@[simp]
+theorem getThe_eq [MonadStateOf σ m] : getThe σ (m := m) = MonadStateOf.get := rfl
 
 @[simp]
 theorem modifyGetThe_eq [MonadStateOf σ m] {α : Type u} (f : σ → α × σ) :
@@ -42,48 +42,54 @@ variable [LawfulMonadStateOf σ m] [LawfulMonad m]
 -- Should this be simp?
 @[simp]
 theorem modifyThe_eq (f : σ → σ) :
-    modifyThe σ (m := m) f = MonadStateOf.get >>= (fun s => set (f s)) := by
+    modifyThe σ (m := m) f = (do set (f (← MonadStateOf.get))) := by
   simp only [modifyThe, modifyGet_eq, bind_pure_comp, id_map']
 
-
 end LawfulMonadStateOf
-
-  -- /-- `modify f` is equivalent to `do set (f (← get))` -/
-  -- modify_eq_set_get : (f : σ → σ) → modify f = bind get (fun s => set (f s))
-  -- /-- `getModify f` is equivalent to `get <* modify f` -/
-  -- getModify_eq_get_seqLeft_modify : (f : σ → σ) → getModify f = seqLeft get (fun _ => modify f)
 
 class LawfulMonadState (σ : outParam (Type u)) (m : Type u → Type v) extends
     Monad m, MonadState σ m : Type (max (u+1) v) where
   /-- `modifyGet f` is equivalent to `do let (a, s) := f (← get); put s; pure a` -/
   modifyGet_eq : {α : Type u} → (f : σ → α × σ) →
     modifyGet f = do let (a, s) := f (← get); set s; pure a
+  -- Is the below the right property?
+  -- get_get : (do let _ ← get; get) = get
 
 export LawfulMonadState (modifyGet_eq)
 
 attribute [simp] LawfulMonadState.modifyGet_eq
 
-  -- /-- `modify f` is equivalent to `do set (f (← get))` -/
-  -- modify_eq_set_get' : (f : σ → σ) → modify f = bind get (fun s => set (f s))
-  -- /-- `getModify f` is equivalent to `get <* modify f` -/
-  -- getModify_eq_get_seqLeft_modify' : (f : σ → σ) → getModify f = seqLeft get (fun _ => modify f)
+namespace LawfulMonadState
 
-variable {σ : Type u} {m : Type u → Type v} [Monad m] [LawfulMonad m] [MonadState σ m] [LawfulMonadState σ m]
+variable {σ : Type u} {m : Type u → Type v} [LawfulMonadState σ m] [LawfulMonad m]
 
 -- @[simp] theorem modifyGet_eq_bind_get_set_pure {α : Type u} (f : σ → α × σ) :
 --     modifyGet f = get >>= (fun s => let p := f s; bind (set p.2) (fun _ => pure p.1)) :=
 --   LawfulMonadState.modifyGet_eq f
 
--- @[simp] theorem modify_eq_set_get (f : σ → σ) :
---     modify f = get >>= (fun s => set (f s)) :=
---   LawfulMonadState.modify_eq_set_get' f
+@[simp]
+theorem modify_eq (f : σ → σ) :
+    modify (m := m) f = (do MonadState.set (f (← get))) := by
+  simp only [modify, modifyGet_eq, bind_pure_comp, id_map']
 
--- @[simp] theorem getModify_eq_get_seqLeft_modify (f : σ → σ) :
---     getModify f = get <* (fun _ => modify f) :=
---   LawfulMonadState.getModify_eq_get_seqLeft_modify' f
+-- Do we need extra property of `get` here? That `get` twice is the same as `get` once?
+@[simp] theorem getModify_eq (f : σ → σ) :
+    getModify (m := m) f = get <* modify f := by
+  simp [getModify, seqLeft_eq_bind]
+  sorry
+
+end LawfulMonadState
 
 section StateT
 
-instance : LawfulMonadStateOf σ (StateT σ m) := sorry
+variable {σ : Type u} {m : Type u → Type v} [Monad m]
+
+instance : LawfulMonadStateOf σ (StateT σ m) where
+  modifyGet_eq f := by
+    simp [MonadStateOf.modifyGet, MonadStateOf.get, MonadStateOf.set, pure]
+    unfold StateT.modifyGet StateT.get StateT.set StateT.pure StateT.instMonad StateT.bind
+    funext s
+    simp
+    sorry
 
 end StateT
