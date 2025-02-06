820c0c098707ec8844f38f3b331a7d1d2d004d80
Quang Dao <quangdao@umich.edu>
2025-02-05 22:10:09 -0500

    update to `RelativeMonad`

--- ToMathlib/Control/RelativeMonad.lean
+++ ToMathlib/Control/RelativeMonad.lean
@@ -115,9 +115,23 @@ def instSeqOfRelativeMonadOfSeq {r : Type u → Type w} {m : Type u → Type v}
     [RelativeMonad r m] [Seq r] : Seq m where
   seq f x := bindᵣ (r := r) (m := m) f (fun y => mapᵣ (y <*> ·) (x ()))
 
-section Lawful
+variable {r : Type u → Type w} {m f : Type u → Type v}
 
-variable {r : Type u → Type w} {m f : Type u → Type v} {α β γ : Type u}
+instance [RelativePure Id f] : Pure f where
+  pure := @pureᵣ Id f _
+
+instance [RelativeBind Id m] : Bind m where
+  bind := @bindᵣ Id m _
+
+instance [RelativeFunctor Id f] : Functor f where
+  map := @mapᵣ Id f _
+  mapConst := @mapConstᵣ Id f _
+
+instance [RelativeMonad Id m] : Monad m where
+  pure := @pureᵣ Id m _
+  bind := @bindᵣ Id m _
+
+section Lawful
 
 class LawfulRelativeFunctor (r : Type u → Type w) (f : Type u → Type v) [RelativeFunctor r f] where
 
@@ -131,6 +145,8 @@ class LawfulRelativeFunctor (r : Type u → Type w) (f : Type u → Type v) [Rel
 export LawfulRelativeFunctor (map_constᵣ id_mapᵣ comp_mapᵣ)
 
 attribute [simp] id_mapᵣ
+
+variable {α β γ : Type u}
 
 @[simp] theorem id_mapᵣ' [RelativeFunctor r f] [LawfulRelativeFunctor r f] (x : f α) :
     (fun a : r α => a) <$>ᵣ x = x :=
@@ -164,5 +180,63 @@ attribute [simp] pure_bindᵣ bind_pure_compᵣ bind_assocᵣ
     x >>=ᵣ pureᵣ (r := r) = x := by
   show x >>=ᵣ (fun a => pureᵣ (id a)) = x
   rw [bind_pure_compᵣ, id_mapᵣ]
+
+theorem map_eq_pure_bindᵣ [RelativeMonad r m] [LawfulRelativeMonad r m]
+    (f : r α → r β) (x : m α) : f <$>ᵣ x = x >>=ᵣ fun a => pureᵣ (f a) := by
+  rw [← bind_pure_compᵣ]
+
+-- theorem seq_eq_bind_mapᵣ [RelativeMonad r m] [LawfulRelativeMonad r m]
+--     (f : m (α → β)) (x : m α) : f <*> x = f >>=ᵣ (. <$>ᵣ x) := by
+--   rw [← bind_mapᵣ]
+
+theorem bind_congrᵣ [RelativeBind r m] {x : m α} {f g : r α → m β} (h : ∀ a, f a = g a) :
+    x >>=ᵣ f = x >>=ᵣ g := by
+  simp [funext h]
+
+theorem bind_pure_unitᵣ [RelativeMonad r m] [LawfulRelativeMonad r m] {x : m PUnit} :
+    (x >>=ᵣ fun y : r PUnit => pureᵣ y) = x := by
+  rw [bind_pure_compᵣ]; exact id_mapᵣ x
+
+theorem map_congrᵣ [RelativeFunctor r m] {x : m α} {f g : r α → r β} (h : ∀ a, f a = g a) :
+    (f <$>ᵣ x : m β) = g <$>ᵣ x := by
+  simp [funext h]
+
+-- theorem seq_eq_bindᵣ [RelativeMonad r m] [LawfulRelativeMonad r m] (mf : m (α → β)) (x : m α) :
+--     mf <*> x = mf >>=ᵣ fun f => f <$>ᵣ x := by
+--   rw [← bind_mapᵣ]
+
+-- theorem seqRight_eq_bind [Monad m] [LawfulMonad m] (x : m α) (y : m β) : x *> y = x >>= fun _ => y := by
+--   rw [seqRight_eq]
+--   simp only [map_eq_pure_bind, const, seq_eq_bind_map, bind_assoc, pure_bind, id_eq, bind_pure]
+
+-- theorem seqLeft_eq_bind [Monad m] [LawfulMonad m] (x : m α) (y : m β) : x <* y = x >>= fun a => y >>= fun _ => pure a := by
+--   rw [seqLeft_eq]
+--   simp only [map_eq_pure_bind, seq_eq_bind_map, bind_assoc, pure_bind, const_apply]
+
+@[simp] theorem map_bindᵣ [RelativeMonad r m] [LawfulRelativeMonad r m]
+    (f : r β → r γ) (x : m α) (g : r α → m β) :
+      f <$>ᵣ (x >>=ᵣ g) = x >>=ᵣ fun a : r α => f <$>ᵣ g a := by
+  rw [← bind_pure_compᵣ, bind_assocᵣ]
+  simp only [bind_pure_compᵣ]
+
+@[simp] theorem bind_map_leftᵣ [RelativeMonad r m] [LawfulRelativeMonad r m]
+    (f : r α → r β) (x : m α) (g : r β → m γ) :
+      ((f <$>ᵣ x) >>=ᵣ fun b => g b) = (x >>=ᵣ fun a : r α => g (f a)) := by
+  rw [← bind_pure_compᵣ, bind_assocᵣ]
+  simp only [pure_bindᵣ]
+
+theorem RelativeFunctor.map_unitᵣ [RelativeMonad r m] [LawfulRelativeMonad r m] {a : m PUnit} :
+    (fun y : r PUnit => y) <$>ᵣ a = a := by
+  simp [map_constᵣ]
+
+instance [RelativeFunctor Id f] [LawfulRelativeFunctor Id f] : LawfulFunctor f where
+  map_const := @map_constᵣ Id f _ _
+  id_map := @id_mapᵣ Id f _ _
+  comp_map := @comp_mapᵣ Id f _ _
+
+-- instance [RelativeMonad Id m] [LawfulRelativeMonad Id m] : LawfulMonad m where
+--   map_const := @map_constᵣ Id m _ _
+--   id_map := @id_mapᵣ Id m _ _
+--   comp_map := @comp_mapᵣ Id m _ _
 
 end Lawful
