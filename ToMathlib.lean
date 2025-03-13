9e922a2407dec1b676cd842e95c4a710c10ccc64
Devon Tuma <dtumad@gmail.com>
2025-03-12 20:50:27 -0500

    add toMathlib line

--- ToMathlib.lean
+++ ToMathlib.lean
@@ -18,4 +18,5 @@ import ToMathlib.Control.RelativeMonad
 import ToMathlib.Control.StateT
 import ToMathlib.Control.WriterT
 import ToMathlib.General
+import ToMathlib.OrderEnrichedCategory
 import ToMathlib.ProbabilityTheory.Coupling
