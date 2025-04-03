58ceffb03e132c198b0f7d98b624d839dff8b987
GabeRobison <gaberobison.mn@gmail.com>
2025-04-03 06:07:31 -0500

    Fixed Examples.lean import order

--- Examples.lean
+++ Examples.lean
@@ -1,6 +1,6 @@
+import Examples.FrankingProtocol
 import Examples.HHS_Elgamal
 import Examples.HHS_Sigma
 import Examples.HHS_Signature
 import Examples.OneTimePad
 import Examples.RF_RP_Switching_alt
-import Examples.FrankingProtocol
--- Examples/FrankingProtocol.lean
+++ Examples/FrankingProtocol.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2024 Gabriel Becker. All rights reserved.
+Copyright (c) 2025 Gabe Robison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gabe Robison
 -/
